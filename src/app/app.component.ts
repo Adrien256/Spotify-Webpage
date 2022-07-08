e27258625d2694a874bc78775507a4a301c8dc63
Adrien <alow5@myseneca.ca>
2022-07-08 16:32:12 -0400

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@
 *
 * Name: Adrien Low Student ID: 137763207 Date: April 8th, 2022
 *
-* Angular App (Deployed) Link: _____________________________________________________
+* Angular App (Deployed) Link: https://resonant-bienenstitch-0c1ce5.netlify.app
 *
 * User API (Heroku) Link: https://ancient-retreat-68858.herokuapp.com/api/user
 *
